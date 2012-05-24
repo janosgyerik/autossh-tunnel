9a8dca97c63b06a4927cf1ebdc3d762663dc1539
Janos Gyerik <janos@xw8400>
2012-05-24 19:24:41 +0200

    made crontab.sh less project dependent and more configurable

--- crontab.sh
+++ crontab.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 
-cd $(dirname $0)
+cd $(dirname "$0")
 
 test "$1" = --remove && mode=remove || mode=add
 
-cron_unique_label="autossh tunnel: $PWD"
+cron_unique_label="# $PWD"
+
+crontab="$0".crontab
+test -f $crontab || cp $crontab.sample $crontab
+
+workfile="$0".tmp
+cleanup() { rm -f "$workfile"; }
+trap 'cleanup; exit 1;' 1 2 3 15
 
 # if crontab is executable
 if type crontab >/dev/null 2>/dev/null; then
     if test $mode = add; then
-	# if this crontab entry is not added yet
-	if ! crontab -l 2>/dev/null | grep "^# $cron_unique_label" >/dev/null 2>/dev/null; then
-	    # creating crontab line
-	    mkdir -p logs
-	    workfile=.$(basename $0)-$$
-	    trap 'rm -f $workfile; exit 1;' 1 2 3 15
-	    cat <<EOF >$workfile
-# $cron_unique_label
-0	*	*	*	*	$PWD/autossh.sh
-
-EOF
-	    echo 'Appending to crontab:'
-	    cat $workfile
-	    crontab -l 2>/dev/null | cat - $workfile | crontab -
-	    rm -f $workfile
-	else
-	    echo 'Crontab entry already exists, skipping ...'
-	fi
+        # if this crontab is not added yet
+        if ! crontab -l 2>/dev/null | grep -x "$cron_unique_label" >/dev/null 2>/dev/null; then
+            export cron_unique_label
+            cat $crontab | perl -ne 's/\$(\w+)/$ENV{$+}/g; print' >$workfile
+            echo 'Appending to crontab:'
+            cat $workfile
+            crontab -l 2>/dev/null | cat - $workfile | crontab -
+        else
+            echo 'Crontab entry already exists, skipping ...'
+            echo
+        fi
+        echo "To remove previously added crontab entry, run: $0 --remove"
+        echo
     elif test $mode = remove; then
-	# if this crontab entry exists
-	if crontab -l 2>/dev/null | grep "^# $cron_unique_label" >/dev/null 2>/dev/null; then
-	    # creating sed file
-	    workfile=.$(basename $0)-$$
-	    trap 'rm -f $workfile; exit 1;' 1 2 3 15
-	    cat <<EOF >$workfile
-\?^# $cron_unique_label? {
+        # if this crontab entry exists
+        if crontab -l 2>/dev/null | grep -x "$cron_unique_label" >/dev/null 2>/dev/null; then
+            # creating sed file
+            cat <<EOF >"$workfile"
+\?^$cron_unique_label? {
 : next
 n
 /./ b next
@@ -42,13 +42,14 @@ d
 }
 p
 EOF
-	    echo Removing crontab entry ...
-	    crontab -l 2>/dev/null | sed -nf $workfile | crontab -
-	    rm -f $workfile
-	else
-	    echo Crontab entry does not exist, nothing to do.
-	fi
+            echo Removing crontab entry ...
+            crontab -l 2>/dev/null | sed -nf "$workfile" | crontab -
+        else
+            echo Crontab entry does not exist, nothing to do.
+        fi
     fi
 fi
+
+cleanup
 
 # eof
