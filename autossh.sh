4a5b388a5933ab1a181af31bc5b968510e56c055
Janos Gyerik <janos@kronos>
2014-07-01 23:59:44 +0200

    handle case of dead sessions

--- autossh.sh
+++ autossh.sh
@@ -12,16 +12,24 @@ test "$1" && tunnelsites="$@" || tunnelsites=$(./confirmed-tunnel-sites.sh)
 # for Mac OS X, otherwise it just doesn't work...
 export AUTOSSH_PORT=0
 
+match_session() {
+    screen -ls | grep -F .$1
+}
+
 for tunnelsite in $tunnelsites; do
     if ! is_confirmed_tunnelsite $tunnelsite; then
         warn tunnelsite: $tunnelsite is not confirmed skipping.
         continue
     fi
-    if ! screen -ls | grep -F .$tunnelsite >/dev/null; then
+
+    # stop running session, unless it matches "Attached" or "Detached"
+    if ! match_session $tunnelsite | awk '$0 !~ /tached/ { exit 1 }'; then
+        ./stop.sh $tunnelsite
+    fi
+
+    if ! match_session $tunnelsite >/dev/null; then
         echo \* starting autossh ...
         screen -d -m -S $tunnelsite autossh -N $tunnelsite
         echo \* done.
     fi
 done
-
-# eof
