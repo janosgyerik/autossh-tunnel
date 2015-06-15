47e0eb4ff32f301b41d2be259be4b60c4966fa33
Marius van Witzenburg <marius@kitara.nl>
2015-06-15 10:52:19 +0200

    Detect location of 'false' on slave OS and put it into public key command

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# 
+#
 # File: ssh-keygen.sh
 # Purpose: create an SSH key without passphrase to use with autossh
 #   in screen, with restrictive options in the public key
@@ -15,7 +15,7 @@ else
     result ssh key file does NOT exist, creating now
     ssh-keygen -t rsa -C $ssh_key_comment -N '' -f $ssh_key_file
     info "adding restrictive options to public key"
-    sed -e "s/^/command=\"\/bin\/false\",no-agent-forwarding,no-X11-forwarding,no-pty /" $ssh_key_file.pub >$ssh_key_file.pub.bak && mv $ssh_key_file.pub.bak $ssh_key_file.pub
+    sed -e "s/^/command=\"__PATH_TO_FALSE__\",no-agent-forwarding,no-X11-forwarding,no-pty /" $ssh_key_file.pub >$ssh_key_file.pub.bak && mv $ssh_key_file.pub.bak $ssh_key_file.pub
 fi
 
 info contents of the public key: $ssh_key_file.pub
